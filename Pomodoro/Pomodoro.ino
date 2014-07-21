6f9723b78def521dad3dc9e6ccf984baff829676
Dinshaw <gobhai@gmail.com>
2014-07-21 12:14:58 -0400

    Done!

--- Pomodoro/Pomodoro.ino
+++ Pomodoro/Pomodoro.ino
@@ -19,12 +19,15 @@ int previous = LOW;    // the previous reading from the input pin
 
 // the follow variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
-long time = 0;         // the last time the output pin was toggled
+long blinkInterval = 500;
+long lastBlink = 0;
+
+long debounceInterval = 200;   // the debounce time, increase if the output flickers
+long debounceStart = 0;         // the last time the output pin was toggled
 long pomodoroStart = 0;
 long breakStart = 0;
-long debounce = 200;   // the debounce time, increase if the output flickers
-long pomodoroTime = 10000; // 10 seconds
-long breakTime = 5000; //  1500000 25 minutes
+long pomodoroTime = 5000; // 10 seconds
+long breakTime = 10000; //  1500000 25 minutes
 boolean onBreak = false;
 boolean onPomodoro = false;
 
@@ -48,11 +51,14 @@ void loop()
   }
 
   if (onBreak == true) {
-    digitalWrite(outPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(500);               // wait for a second
-    digitalWrite(outPin, LOW);    // turn the LED off by making the voltage LOW
-    delay(500);
-
+    long currentMillis = millis();
+    if (currentMillis - lastBlink > blinkInterval) {
+      lastBlink = currentMillis;
+      if (state == LOW)
+        state = HIGH;
+      else
+        state = LOW;
+    }
     if (millis() - breakStart > breakTime) {
       onBreak = false;
       state = LOW;
@@ -63,9 +69,11 @@ void loop()
   // if the input just went from LOW and HIGH and we've waited long enough
   // to ignore any noise on the circuit, toggle the output pin and remember
   // the time
-  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-    if (state == HIGH) {
+  if (reading == HIGH && previous == LOW && millis() - debounceStart > debounceInterval) {
+    if (onPomodoro == true || onBreak == true) {
       state = LOW;
+      onPomodoro = false;
+      onBreak = false;
     }
     else
     {
@@ -74,7 +82,7 @@ void loop()
       pomodoroStart = millis();
     }
 
-    time = millis();
+    debounceStart = millis();
   }
 
   digitalWrite(outPin, state);
